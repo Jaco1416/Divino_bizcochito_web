cbe2ee2a0c288ea4587074ba93e8dbdaff99d5a7
Joaquín Antonio Galarce Núñez <129196766+Jaco1416@users.noreply.github.com>
2025-11-01 21:47:47 -0300

    ADSASDASDSADASDASD
    sssss

--- divino_bizcochito/src/app/admin/page.tsx
+++ divino_bizcochito/src/app/admin/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import ProtectedRoute from "@/app/components/ProtectedRoute/ProtectedRoute";
+import ProtectedRoute from "@/app/components/protectedRoute/ProtectedRoute";
 
 export default function AdminPage() {
   return (
